1422aaf06b369c1f87b3bd394b8644c7d7073f13
Evan <eahedges2@gmail.com>
2025-11-23 23:06:39 -0500

    Daily leetcode.1262

---
+++ medium/1262. Greatest Sum Divisible by Three.js
@@ -0,0 +1,34 @@
+var maxSumDivThree = function(nums) {
+    let smallestOne = Infinity;
+    let secondSmallestOne = Infinity;
+    let smallestTwo = Infinity;
+    let secondSmallestTwo = Infinity;
+    let total = 0;
+    for(let i = 0; i<nums.length;i++)
+    {
+        total += nums[i];
+        const remain = nums[i]%3;
+        if(remain == 0) continue;
+        else if(remain == 2)
+        {
+            if(nums[i]<=smallestTwo)
+            {
+                secondSmallestTwo = smallestTwo;
+                smallestTwo = nums[i];
+            }
+            else if(nums[i]<=secondSmallestTwo)
+            {
+                secondSmallestTwo = nums[i];
+            }
+        }
+        else if(nums[i] <= smallestOne)
+        {
+            secondSmallestOne = smallestOne;
+            smallestOne = nums[i];
+        }
+        else if(nums[i] < secondSmallestOne) secondSmallestOne = nums[i];
+    }
+
+    if(total%3 == 0) return total;
+    if(total%3 == 1) return total - Math.min(smallestTwo+secondSmallestTwo,smallestOne);
+    return total - Math.min(smallestOne+secondSmallestOne,smallestTwo);
